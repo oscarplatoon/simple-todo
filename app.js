b6e61f79d20284b470211151bff97956b57c7848
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-17 17:50:37 -0400

    Fixed one persistent list item error.

--- app.js
+++ app.js
@@ -15,23 +15,29 @@ const addItem = () => {
   if(toDoItem.length === 0) {
     return null
   }
-  localStorage.setItem('item '+(numberItems+1), toDoItem)
+  localStorage.setItem('item '+(numberItems), toDoItem)
 }
 
 
-//
+const clearList = () => {}
+
 const updateList = ()=> {
   let unorderedList = document.getElementById('todo-list')
+
   // Empties the html list of old items.
   if (unorderedList) {
     while(unorderedList.firstChild) {
       unorderedList.removeChild(unorderedList.firstChild)
     }
   }
-  
+
+  if(localStorage.length == 0) {
+    return
+  }
+  console.log(localStorage)
   // Begin building the list items from storedItems array to append back to the ul
-  
-  for(let i = 0; i <= localStorage.length; i++) {
+  for(let i = 0; i < localStorage.length; i++) {
+    console.log(`Adding item ${i} from local storage`)
     //This builds the LI to add to the list
     let newListItem = document.createElement("li")
     let newCheckBox = document.createElement('input')
@@ -50,8 +56,18 @@ const updateList = ()=> {
     newPara = document.createElement('p')
     newPara.setAttribute('id', `para ${i}`)
     
-    newText = (document.createTextNode(localStorage.getItem(`item ${i}`)) || (document.createTextNode(localStorage.getItem(`item ${i} checked`))))
+    newText = (document.createTextNode(localStorage.getItem(`item ${i}`)))
+    if(newText.length == 0) {
+      newText = document.createTextNode(localStorage.getItem(`item ${i} checked`))
+    }
+    
     // Check if the key is "checked" and if so, strikeText for that block.
+    if(localStorage.key(i) == `item ${i} checked`) {
+      newCheckBox.checked = true;
+    } else if(localStorage.key(i) == `item ${i}`) {
+      newCheckBox.checked = false;
+    }
+    
     newPara.appendChild(newText)
     newListItem.appendChild(newCheckBox)
     newListItem.appendChild(newPara)
@@ -63,6 +79,7 @@ const strikeText = (id) => {
   strikePara = document.getElementById(`para ${id}`)
   strikePara.style.textDecoration = 'line-through'
   storageVal = localStorage.getItem(`item ${id}`)
+  console.log(storageVal)
   localStorage.setItem(`item ${id} checked`, storageVal)
   localStorage.removeItem(`item ${id}`)
 
@@ -71,7 +88,10 @@ const strikeText = (id) => {
 const normalText = (id) => {
   normalPara = document.getElementById(`para ${id}`)
   normalPara.style.textDecoration = 'none'
-
+  storageVal = localStorage.getItem(`item ${id} checked`)
+  console.log(storageVal)
+  localStorage.setItem(`item ${id}`, storageVal)
+  localStorage.removeItem(`item ${id} checked`)
 }
 
 updateList()
